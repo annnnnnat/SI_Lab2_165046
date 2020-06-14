547806a3f841518521845c685ccf6ec7fb72a571
Ana <58486697+annnnnnat@users.noreply.github.com>
2020-06-14 03:41:45 +0200

    Finished project

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # SI_Lab2_165046
+##Ана Тошева, индекс 165046
+###Група на код:
+Ја добив групата на код 4
+<br />  
+###Control Flow Graph
+#![Screenshot](SI_lab2_165046\CFG.jpg)
+###Цикломатска комплексност
+Цикломатска комплекност е 7 
+<br/>
+N=19, E=24 => E-N+2=7,
+каде N e бројот на јазли,а E е бројот на рабови<br /> 
+
+###Тест случаи според критериумот Every Path
+1.user=null <br/>
+2.username=null password=null <br/>
+3.username=Ana password=AnA123 <br/>
+4.username=anything password=abc12233@ (не го исполнува условот за голема буква)<br/>
+5.username=anything password=abc12233D (не го исполнува условот за специјален силмбол)<br/>
+6.username=anything password=123.@$243 (не го исполнува условот за буква)<br/>
+Минимален број на test cases = 6
+###Тест случаи според критериумот Multiple conditions
+1.if (user.getUsername()!=null && user.getPassword()!=null)
+T&&T -> username !=null password !=null
+T&&F -> username=anything password=null
+F&&X -> username = null password=anything
+2.if (!passwordLower.contains(user.getUsername().toLowerCase()) && password.length()>=8)
+T&&T -> username is not contained in pass and passwords length >=8
+T&&F -> username is not contained in pass and passwords length < 8
+F&&X -> username contained in pass and password=anything
+3.if (digit && upper && special)
+T&&T&&T -> password contains digit,upper case and special sign
+T&&T&&F -> password contains digit and upper case but not special sign
+T&&F&&T ->password contains digit and special sign but not upper case 
+F&&X&&X -> password does not contain a digit
+###Објаснување на напишаните unit tests
+1.Every Path
+Избрани се test cases што ќе ја поминат секоја патека во графот.
+Во овој случај се 6.
+2.Multiple conditions
+Избрани се test cases за секој if кој содржи повеќе од еден услов.
+Во кој се разгледува секоја можна комбинација на True or/and False
--- SI_lab2_165046/src/test/java/SILab2Test.java
+++ SI_lab2_165046/src/test/java/SILab2Test.java
@@ -1,2 +1,89 @@
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SILab2Test {
+    private final SILab2 l = new SILab2();
+    String username = new String();
+    String password = new String();
+    String email = new String();
+    private final User u = new User(username,password,email);
+    private List<String> allUsers(String... elems){
+        return new ArrayList<>(Arrays.asList(elems));
+    };
+    @Test
+    public void testEveryPath(){
+        //user=null
+        assertEquals(false,l.function(null,allUsers()));
+        //username=null password=null
+        u.username=null;
+        u.password=null;
+        assertEquals(false,l.function(u,allUsers()));
+        //username=Ana password=AnA123
+        u.username="Ana";
+        u.password="AnA123";
+        assertEquals(false,l.function(u,allUsers()));
+        //username=anything password=abc12233@
+        u.password="abc12233@";
+        assertEquals(false,l.function(u,allUsers()));
+        //username=anything password=abc12233D
+        u.password="abc12233D";
+        assertEquals(false,l.function(u,allUsers()));
+        //username=anything password=123.@$243
+        u.password="123.@$243";
+        assertEquals(false,l.function(u,allUsers()));
+        //username=anything password=aBC24.!6b
+        u.password="aBC24.!6b";
+        assertEquals(true,l.function(u,allUsers()));
+    }
+    @Test
+    public void multipleConditions(){
+        Exception ex;
+        //if (user.getUsername()!=null && user.getPassword()!=null)
+            //T&&T -> username !=null password !=null
+            u.username="dasda";
+            u.password="dsada";
+            assertFalse(l.function(u,allUsers()));
+            //T&&F -> username=anything password=null
+            u.username="fsdfds";
+            u.password=null;
+            assertFalse(l.function(u,allUsers()));
+            //F&&X -> username = null password=anything
+            u.username=null;
+            u.password="sdasda";
+            assertFalse(l.function(u,allUsers()));
+
+        //if (!passwordLower.contains(user.getUsername().toLowerCase()) && password.length()>=8)
+            //T&&T -> username is not contained in pass and passwords lenght >=8
+            u.username="Ana";
+            u.password="A123$.231b";
+            assertTrue(l.function(u,allUsers()));
+            //T&&F -> username is not contained in pass and passwords length < 8
+            u.username="Ana";
+            u.password="sda";
+            assertFalse(l.function(u,allUsers()));
+            //F&&X -> username contained in pass and password=anything
+            u.username="Ana";
+            u.password="ana";
+            assertFalse(l.function(u,allUsers()));
+
+        //if (digit && upper && special)
+            //T&&T&&T -> password contains digit,upper case and special sign
+            u.username = "Ana";
+            u.password = "AbC232.$dsa";
+            assertTrue(l.function(u,allUsers()));
+            //T&&T&&F -> password contains digit and upper case but not special sign
+            u.password = "AbC232dsa";
+            assertFalse(l.function(u,allUsers()));
+            //T&&F&&T ->password contains digit and special sign but not upper case
+            u.password = "b23$/2dsa";
+            assertFalse(l.function(u,allUsers()));
+            //F&&X&&X -> password does not contain a digit
+            u.password = "AbC$$$dsa";
+            assertFalse(l.function(u,allUsers()));
+    }
+
 }
